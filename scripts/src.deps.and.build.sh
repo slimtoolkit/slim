8da89601ea2d4d7667d514dc3daeab0b482993b4
Kyle Quest <kcq.code@cloudimmunity.net>
2016-10-10 00:11:49 -0700

    use the same build flags in src.build.sh and src.deps.and.build.sh

--- scripts/src.deps.and.build.sh
+++ scripts/src.deps.and.build.sh
@@ -24,8 +24,15 @@ go get -d bitbucket.org/madmo/fanotify
 go get github.com/docker/go-connections/nat
 popd
 pushd $BDIR/apps/docker-slim
-gox -osarch="linux/amd64" -output="$BDIR/bin/linux/docker-slim"
-gox -osarch="darwin/amd64" -output="$BDIR/bin/mac/docker-slim"
+build_time="$(date -u '+%Y-%m-%d_%I:%M:%S%p')"
+tag="current"
+revision="current"
+if hash git 2>/dev/null && [ -e $BDIR/.git ]; then
+  tag="$(git describe --tags)"
+  revision="$(git rev-parse HEAD)"
+fi
+gox -osarch="linux/amd64" -output="$BDIR/bin/linux/docker-slim" -ldflags="-X main.appVersionTag=$tag -X main.appVersionRev=$revision -X main.appVersionTime=$build_time"
+gox -osarch="darwin/amd64" -output="$BDIR/bin/mac/docker-slim" -ldflags="-X main.appVersionTag=$tag -X main.appVersionRev=$revision -X main.appVersionTime=$build_time"
 popd
 pushd $BDIR/apps/docker-slim-sensor
 gox -osarch="linux/amd64" -output="$BDIR/bin/linux/docker-slim-sensor"
