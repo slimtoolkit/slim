2bb1c809991ecea496e5bb6c3c58232d22cc7b84
Csaba Kiraly <csaba.kiraly@gmail.com>
2016-10-10 02:38:23 +0200

    docker builder: fix missing dependence
    Signed-off-by: Csaba Kiraly <csaba.kiraly@gmail.com>

--- scripts/src.deps.and.build.sh
+++ scripts/src.deps.and.build.sh
@@ -21,6 +21,7 @@ go get github.com/franela/goreq
 go get github.com/go-mangos/mangos
 go get github.com/dustin/go-humanize
 go get -d bitbucket.org/madmo/fanotify
+go get github.com/docker/go-connections/nat
 popd
 pushd $BDIR/apps/docker-slim
 gox -osarch="linux/amd64" -output="$BDIR/bin/linux/docker-slim"
