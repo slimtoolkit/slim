decf6d429ab1412c5597d757bf8e736c98dc7e8f
Kyle Quest <kcq.public@gmail.com>
2019-11-17 17:10:46 -0800

    added certs to the docker-slim container image

--- build/package/docker/build.sh
+++ build/package/docker/build.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-docker build --squash -t docker-slim -f Dockerfile ../../..
-
+docker build --squash --rm -t docker-slim -f Dockerfile ../../..
+docker image prune --filter label=build-role=ca-certs -f
+docker image prune --filter label=app=docker-slim -f
