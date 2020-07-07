12c2f5488ae34fc03bac3c961976e12b4ad45595
Kyle Quest <kcq.public@gmail.com>
2020-07-07 07:29:42 -0700

    cleanup script enhancements

--- scripts/src.cleanup.sh
+++ scripts/src.cleanup.sh
@@ -11,6 +11,8 @@ rm -rfv $BDIR/dist_linux
 rm -fv $BDIR/dist_linux.tar.gz
 rm -rfv $BDIR/dist_linux_arm
 rm -fv $BDIR/dist_linux_arm.tar.gz
+rm -rfv $BDIR/dist_linux_arm64
+rm -fv $BDIR/dist_linux_arm64.tar.gz
 rm -rfv $BDIR/dist_mac
 rm -fv $BDIR/dist_mac.zip
 rm -rfv $BDIR/bin
