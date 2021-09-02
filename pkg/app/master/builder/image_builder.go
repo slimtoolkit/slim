404603da29beb07e001d32e51ce0ebf745d06ee8
Ian Juma <wjuma.do@gmail.com>
2021-09-02 21:25:52 +0300

    add image platform from image inspector on minified image build

--- pkg/app/master/builder/image_builder.go
+++ pkg/app/master/builder/image_builder.go
@@ -11,7 +11,7 @@ import (
 	"github.com/docker-slim/docker-slim/pkg/docker/dockerfile/reverse"
 	"github.com/docker-slim/docker-slim/pkg/util/fsutil"
 
-	"github.com/fsouza/go-dockerclient"
+	docker "github.com/fsouza/go-dockerclient"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -170,15 +170,22 @@ func NewImageBuilder(client *docker.Client,
 		}
 	}
 
+	var platform string
+	if imageInfo != nil && imageInfo.OS != "" && imageInfo.Architecture != "" {
+		platform = fmt.Sprintf("%s/%s", imageInfo.OS, imageInfo.Architecture)
+	}
+
 	builder := &ImageBuilder{
 		BasicImageBuilder: BasicImageBuilder{
 			ShowBuildLogs: showBuildLogs,
 			APIClient:     client,
+			// extract platform - from image inspector
 			BuildOptions: docker.BuildImageOptions{
 				Name:           imageRepoNameTag,
 				RmTmpContainer: true,
 				ContextDir:     artifactLocation,
 				Dockerfile:     "Dockerfile",
+				Platform:       platform,
 				//SuppressOutput: true,
 			},
 		},
