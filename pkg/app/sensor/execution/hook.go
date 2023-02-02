45d45a539d6f64a7addc1de95884ebc703f19bbc
Ivan Velichko <iximiuz@gmail.com>
2023-02-02 15:43:11 +0000

    sensor: Too fast lifecycle hook handlers cause false positive error conditions
    exec.Command().Run() and its derivatives are prone to a race condition
    between the underlying cmd.Start() + cmd.Wait() calls and the actual
    child process execution. Too fast commands can cause the Wait() call
    being called when the child process is already gone. This makes the
    Run() method to return an error. This PR makes sensor ignore such
    errors.

--- pkg/app/sensor/execution/hook.go
+++ pkg/app/sensor/execution/hook.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os/exec"
 
+	"github.com/docker-slim/docker-slim/pkg/util/errutil"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -49,16 +50,17 @@ func (h *hookExecutor) doHook(k kind) {
 
 	cmd := exec.CommandContext(h.ctx, h.cmd, string(k))
 	out, err := cmd.CombinedOutput()
-	if err == nil {
-		log.
-			WithField("kind", k).
-			WithField("output", string(out)).
-			Info("lifecycle hook command succeeded")
+
+	logger := log.
+		WithField("kind", k).
+		WithField("command", h.cmd).
+		WithField("exit_code", cmd.ProcessState.ExitCode()).
+		WithField("output", string(out))
+
+	// Some lifecycle hooks are really fast - hence, the IsNoChildProcesses() check.
+	if err == nil || errutil.IsNoChildProcesses(err) {
+		logger.Info("lifecycle hook command succeeded")
 	} else {
-		log.
-			WithError(err).
-			WithField("kind", k).
-			WithField("output", string(out)).
-			Info("lifecycle hook command failed")
+		logger.WithError(err).Info("lifecycle hook command failed")
 	}
 }
